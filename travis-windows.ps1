343b2dcf8cd3587fd51d6a68e8af252d049563bc
assertivist <andy.halstead@gmail.com>
2019-02-25 05:34:55 -0700

    Run 'make tests' on Travis within testing branch

--- travis-windows.ps1
+++ travis-windows.ps1
@@ -1,4 +1,4 @@
 Set-Alias bash "C:\tools\msys64\usr\bin\bash.exe"
 bash -lc 'pacman --noconfirm --needed -Syuu zip mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-clang mingw-w64-x86_64-glbinding mingw-w64-x86_64-glew make'
-bash -lc 'PATH=$PATH:/mingw64/bin; echo $PATH; make winapp'
+bash -lc 'PATH=$PATH:/mingw64/bin; echo $PATH; make winapp; make tests'
 Exit $LASTEXITCODE
