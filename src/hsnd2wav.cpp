f3edf0983154844b8853cb98975ecc8c8bba3faa
assertivist <andy.halstead@gmail.com>
2020-06-07 02:10:42 -0600

    Sometimes loopEnd - loopStart is still zero even if the values themselves are not zero, don't add loopStart to sample data offset unless loop length is > 0

--- src/hsnd2wav.cpp
+++ src/hsnd2wav.cpp
@@ -32,12 +32,13 @@ int main(int argc, char *argv[]) {
     if (sample) {
         SampleHeaderPtr sp = *sample;
         // use loop length if it exists
+        bool loops = true;
         int len = sp->loopEnd - sp->loopStart;
         // else use the total sample length
         if (len < 1) {
+            loops = false;
             len = sp->len;
         }
-        SDL_Log("Length: %i", len);
         unsigned char *p = sizeof(SampleHeader) + (unsigned char *)sp;
 
         AudioFile<float> audioFile; // defaults to 16-bit, 44100hz
@@ -45,8 +46,11 @@ int main(int argc, char *argv[]) {
         audioFile.setSampleRate(ToFloat(sp->baseRate) * 22254.54545);
         audioFile.setAudioBufferSize(1, len); // 1 channel, num samples
         for (int i = 0; i < len; i++) {
-            audioFile.samples[0][i] = p[i+sp->loopStart] / 255.0;
             //audioFile.samples[0][i] = (p[i+sp->loopStart] * 32767) / (0xFF >> (8-BITSPERSAMPLE));
+            if (loops)
+                audioFile.samples[0][i] = p[i+sp->loopStart] / 255.0;
+            else
+                audioFile.samples[0][i] = p[i] / 255.0;        
         }
         audioFile.save(argv[2]);
     }
