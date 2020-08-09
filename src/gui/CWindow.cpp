c13f984a33827f970a291bf156f7b31abf782feb
Blenkush <blenkush@gmail.com>
2020-08-08 19:27:16 -0700

    scoreboard and save window position
    Show an in game scoreboard and save position of windows

--- src/gui/CWindow.cpp
+++ src/gui/CWindow.cpp
@@ -10,3 +10,15 @@ CWindow::CWindow(CApplication *app, const std::string &title) :
 CWindow::~CWindow() {
     mApplication->Unregister(this);
 }
+
+void CWindow::restoreState() {
+    long x = mApplication->Number(title() + "X", 50);
+    long y = mApplication->Number(title() + "Y", 50);
+    setPosition(nanogui::Vector2i(x, y));
+}
+
+void CWindow::saveState() {
+    nanogui::Vector2i position = this->position();
+    mApplication->Set(title() + "X", position.x);
+    mApplication->Set(title() + "Y", position.y);
+}
