a3a48f4c44461f29d08431b38c7eedbf6608b4dc
Dan Watson <dcwatson@gmail.com>
2021-02-24 20:25:11 -0500

    Dumb rounding gets smarter

--- bin/pict2alf.py
+++ bin/pict2alf.py
@@ -19,10 +19,26 @@ def debug(fmt, *args, **kwargs):
         print(fmt % args, file=sys.stderr, flush=True)
 
 
-def dumb_round(s, zeros=6):
-    # 10.00000007 => 10
-    # 4.500000022 => 4.5
-    return str(s).split("0" * zeros, 1)[0].rstrip(".")
+def dumb_round(num, repeats=6):
+    s = str(num)
+    if "." not in s:
+        return s
+    zeros = "0" * repeats
+    nines = "9" * repeats
+    if zeros in s:
+        return s.split(zeros, 1)[0].rstrip(".")
+    if nines in s:
+        # How many digits before the nines is how many we want to round to.
+        places = len(s.split(nines, 1)[0].split(".")[1]) or None
+        return str(round(num, places))
+    return s
+
+
+# How's this for continuous integration.
+assert dumb_round(10.00000007) == "10"
+assert dumb_round(4.500000022) == "4.5"
+assert dumb_round(0.999999987) == "1", dumb_round(0.999999987)
+assert dumb_round(0.049999999) == "0.05"
 
 
 class Rect:
