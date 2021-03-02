92f4fc32483160ad88aa0e5e85e0c2bc89d5b488
Dan Watson <dcwatson@gmail.com>
2021-03-01 21:50:14 -0500

    Tweak leg motion (refs #19) and collisions (refs #152)

--- src/bsp/CSmartPart.cpp
+++ src/bsp/CSmartPart.cpp
@@ -302,8 +302,10 @@ void CSmartPart::RayTest(RayHitRecord *hitRec) {
                             whichPlane = i;
                         }
                     }
-
-                    if (boxDist >= 0 && boxDist < hitRec->distance) {
+                    // Original was boxDist < hitRec->distance
+                    // Changed because some zero-height walls on the ground were not
+                    // triggering collisions (boxDist == hitRec->distance).
+                    if (boxDist >= 0 && boxDist <= hitRec->distance) {
                         Fixed coord;
                         for (i = 0; i < NUMDIM; i++) {
                             if (i != whichPlane) {
