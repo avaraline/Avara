e96b8f1009600056284bc32f783b42d70e701632
Dan Watson <dcwatson@gmail.com>
2021-03-15 21:18:11 -0400

    Revert "Parallelize convert_all"
    This reverts commit 3e3d7fef3695d1726a2215f5fe7fc2b6f3cd804e.

--- bin/convert_all.py
+++ bin/convert_all.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 from rsrc2files import convert_to_files
-import multiprocessing
 
 import sys
 import os
 
-ldir = "levels"
+ldir = 'levels'
 
-
-if __name__ == "__main__":
-    paths = []
+if __name__ == '__main__':  
     for rsrc_file in os.listdir(ldir):
         rpath = os.path.join(ldir, rsrc_file)
         if os.path.isdir(rpath) or not str(rpath).endswith(".r"):
             continue
         lpath = os.path.join(ldir, rsrc_file.replace(".r", ""))
-        paths.append((rpath, lpath))
-    with multiprocessing.Pool() as pool:
-        pool.starmap(convert_to_files, paths)
+        print(f"{rpath} -> {lpath}")
+        convert_to_files(rpath, lpath)
