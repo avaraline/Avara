3e3d7fef3695d1726a2215f5fe7fc2b6f3cd804e
Dan Watson <dcwatson@gmail.com>
2021-03-15 21:15:58 -0400

    Parallelize convert_all

--- bin/convert_all.py
+++ bin/convert_all.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 from rsrc2files import convert_to_files
+import multiprocessing
 
 import sys
 import os
 
-ldir = 'levels'
+ldir = "levels"
 
-if __name__ == '__main__':  
+
+if __name__ == "__main__":
+    paths = []
     for rsrc_file in os.listdir(ldir):
         rpath = os.path.join(ldir, rsrc_file)
         if os.path.isdir(rpath) or not str(rpath).endswith(".r"):
             continue
         lpath = os.path.join(ldir, rsrc_file.replace(".r", ""))
-        print(f"{rpath} -> {lpath}")
-        convert_to_files(rpath, lpath)
+        paths.append((rpath, lpath))
+    with multiprocessing.Pool() as pool:
+        pool.starmap(convert_to_files, paths)
