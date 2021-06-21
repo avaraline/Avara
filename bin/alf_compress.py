209e5e2d2fa3efe8ef307eef29b3e61110353686
Ryan Herriman <rherriman@gmail.com>
2021-06-21 14:23:27 -0600

    Fix zero-height walls in alf_compress.py

--- bin/alf_compress.py
+++ bin/alf_compress.py
@@ -12,18 +12,24 @@ if __name__ == '__main__':
     parser.add_argument('file', type=open)
 
     args = parser.parse_args()
-    lastWallHeight = 0
+    last_wallHeight = 0
+    did_output_wallHeight_zero = False
     to_remove = []
     root = ET.fromstring(args.file.read())
     for child in root:
         if child.tag == 'set':
             if 'wallHeight' in child.attrib:
-                lastWallHeight = float(child.attrib.pop('wallHeight'))
-                if len(child.attrib) == 0:
-                    to_remove.append(child)
+                last_wallHeight = float(child.attrib['wallHeight'])
+                if last_wallHeight != 0 or did_output_wallHeight_zero:
+                    if len(child.attrib) == 1:
+                        to_remove.append(child)
+                    else:
+                        del child.attrib['wallHeight']
+                else:
+                    did_output_wallHeight_zero = True
         elif child.tag in ['Wall', 'WallSolid', 'FreeSolid', 'WallDoor', 'Field']:
             if 'h' in child.attrib and child.attrib['h'] == '0':
-                child.attrib['h'] = dumb_round(lastWallHeight)
+                child.attrib['h'] = dumb_round(last_wallHeight)
     for child in to_remove:
         root.remove(child)
     print(ET.tostring(root, encoding="unicode"))
