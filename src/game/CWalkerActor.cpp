avaraline/Avara

commit 1a0479fded6bd06e9426a69257ef053463cf17ad

new utility function, ClassicCoefficient2()
occasionally you have to convert back to Classic units

src/game/CWalkerActor.cpp
@@ -435,7 +435,7 @@ void CWalkerActor::DoLegTouches() {
             }
         } else {
             for (i = 0; i < 2; i++) {
-                if (power[i] / itsGame->fpsScale > FIX3(10) && soundId[i]) {
+                if (power[i] > FpsCoefficient2(FIX3(10)) && soundId[i]) {
                     DoSound(soundId[i], legs[i].where, power[i], FIX(1));
                 }
             }
@@ -495,7 +495,7 @@ void CWalkerActor::MoveLegs() {
     headHeight = elevation + FMul(viewPitch, FIX3(10));
     scoutBaseHeight = elevation + SCOUTPLATFORM;
 
-    temp = FMul(-LEGSPACE * 18, headChange / itsGame->fpsScale);
+    temp = FMul(-LEGSPACE * 18, FpsCoefficient2(headChange));
     legSpeeds[0] = distance - temp;
     legSpeeds[1] = distance + temp;
     FPS_DEBUG("distance = " << distance << ", temp = " << temp << ", headChange = " << headChange << "\n");

src/game/CAbstractActor.cpp
@@ -1249,3 +1249,22 @@ Fixed CAbstractActor::FpsOffset(Fixed classicCoeff2) {
 double CAbstractActor::FpsCoefficient1(double fpsCoeff1, double fpsScale) {
     return pow(fpsCoeff1, fpsScale);
 }
+
+long CAbstractActor::FpsFramesPerClassic(long classicFrames)
+{
+    if (HandlesFastFPS()) {
+        return long(classicFrames / itsGame->fpsScale);
+    } else {
+        return 1;
+    }
+}
+
+// basically the inverse of FpsCoefficient2... convert FPS values to Classic units
+Fixed CAbstractActor::ClassicCoefficient2(Fixed fpsValue) {
+    if (HandlesFastFPS()) {
+        // lround rounds both positive and negative values away from zero (adding 0.5 doesn't)
+        return std::lround(fpsValue / itsGame->fpsScale);
+    } else {
+        return fpsValue;
+    }
+}

src/game/CAbstractActor.h
@@ -230,7 +230,8 @@ public:
     Fixed FpsCoefficient1(Fixed classicMultiplier1);
     Fixed FpsCoefficient2(Fixed classicMultiplier2);
     Fixed FpsOffset(Fixed classicCoeff2);
-    long FpsFramesPerClassic(long classicFrames = 1) { return long(classicFrames / itsGame->fpsScale); }
+    long FpsFramesPerClassic(long classicFrames = 1);
+    Fixed ClassicCoefficient2(Fixed fpsValue);
 private:
     virtual double FpsCoefficient1(double classicCoeef1, double fpsScale);
 };

src/game/CFreeSolid.cpp
@@ -167,10 +167,11 @@ void CFreeSolid::FrameAction() {
         FPS_DEBUG("CFreeSolid speed = " << FormatVector(speed, 3) << "\n");
 
         //	Check for ground level (not done by regular collision checks
+        Fixed fallSpeed = ClassicCoefficient2(-partList[0]->minBounds.y - location[1]);
         if ( // speed[1] < 0 &&
-            location[1] + speed[1] * itsGame->fpsScale < -partList[0]->minBounds.y) { //	Bounce up on ground hit.
-            speed[1] = (-partList[0]->minBounds.y - location[1]) / itsGame->fpsScale;
-            FPS_DEBUG("  ground level speed = " << FormatVector(speed, 3) << "\n");
+            speed[1] < fallSpeed) { //	Bounce up on ground hit.
+            speed[1] = fallSpeed;
+            FPS_DEBUG("  using fallSpeed = " << fallSpeed << "\n");
         }
 
         //	If we are moving fast enough, translate in 3D.

src/game/CPlayerManager.cpp
@@ -329,7 +329,6 @@ void CPlayerManagerImpl::HandleEvent(SDL_Event &event) {
 
 void CPlayerManagerImpl::SendFrame() {
     // Sends the next game frame.
-    // itsGame->topSentFrame += (1 / itsGame->fpsScale);
     itsGame->topSentFrame += 1;
 
     // uint32_t ffi = itsGame->topSentFrame * itsGame->fpsScale;

commit e3a85cec75734915d68c1b9b7450800d8b14e013

fixed leg phase bug accidentally introduced in 1a0479fde

src/game/CWalkerActor.cpp
@@ -495,7 +495,7 @@ void CWalkerActor::MoveLegs() {
     headHeight = elevation + FMul(viewPitch, FIX3(10));
     scoutBaseHeight = elevation + SCOUTPLATFORM;
 
-    temp = FMul(-LEGSPACE * 18, FpsCoefficient2(headChange));
+    temp = FMul(-LEGSPACE * 18, ClassicCoefficient2(headChange));
     legSpeeds[0] = distance - temp;
     legSpeeds[1] = distance + temp;
     FPS_DEBUG("distance = " << distance << ", temp = " << temp << ", headChange = " << headChange << "\n");