96133a4ca57d0a9465ffd8e73cc5a4287652ca35
Blenkush <blenkush@gmail.com>
2022-06-22 20:47:47 -0700

    status bug fix

--- src/net/CProtoControl.cpp
+++ src/net/CProtoControl.cpp
@@ -124,7 +124,7 @@ Boolean CProtoControl::DelayedPacketHandler(PacketInfo *thePacket) {
             break;
         case kpPlayerStatusChange:
             theNet->ReceivePlayerStatus(
-                thePacket->p1, thePacket->p2, thePacket->p3, *(long *)thePacket->dataBuffer);
+                thePacket->sender, thePacket->p2, thePacket->p3, *(long *)thePacket->dataBuffer);
             break;
         case kpJSON:
             theNet->ReceiveJSON(
