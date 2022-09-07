42cd88ce856b926e2fd67e2c79bd812678785d31
Tom Anderson <tom@squeat.com>
2022-09-06 18:10:51 -0600

    fixed ability to run multiple games
    all you have to do is make sure to set loaderSlot to the person loading the level then CNetManager::LevelLoadStatus() takes care of the rest.

--- src/game/CNetManager.cpp
+++ src/game/CNetManager.cpp
@@ -413,14 +413,13 @@ void CNetManager::HandleDisconnect(short slotId, short why) {
 void CNetManager::SendLoadLevel(std::string theSet, std::string levelTag, int16_t originalSender /* default = 0 */) {
     CAvaraApp *theApp;
     PacketInfo *aPacket;
+    SDL_Log("SendLoadLevel(%s, %s, %d)\n", theSet.c_str(), levelTag.c_str(), originalSender);
 
     ProcessQueue();
 
     aPacket = itsCommManager->GetPacket();
 
     theApp = itsGame->itsApp;
-
-    SDL_Log("SendLoadLevel(%s, %s, %d)\n", theSet.c_str(), levelTag.c_str(), originalSender);
 
     aPacket->command = kpLoadLevel;
     aPacket->p1 = 0;
@@ -477,6 +476,8 @@ void CNetManager::ReceiveLoadLevel(short senderSlot, int16_t originalSender, cha
             SendLoadLevel(set, tag, senderSlot);
         }
     } else if (!isPlaying) {
+        // save off the player that originally loaded this level to help loading side games
+        loaderSlot = originalSender;
         CPlayerManager *sendingPlayer = playerTable[originalSender];
 
         theApp = itsGame->itsApp;
