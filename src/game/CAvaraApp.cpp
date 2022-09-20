05c11b00d237c1d73fd86f0a85f783a02c061518
Tom Anderson <tom@squeat.com>
2022-09-20 17:44:41 -0600

    Revert "auto-load BD3 while testing this branch"
    This reverts commit 560ea9746bbe9f9e4df1daa083a7077c7c43b9f9.

--- src/game/CAvaraApp.cpp
+++ src/game/CAvaraApp.cpp
@@ -117,8 +117,7 @@ CAvaraAppImpl::CAvaraAppImpl() : CApplication("Avara") {
     AddMessageLine("Type /help and press return for a list of chat commands.");
 
     // load up a random decent starting level
-    // itsTui->ExecuteMatchingCommand("/rand -normal -tre -strict emo", CPlayerManagerImpl::LocalPlayer());
-    itsTui->ExecuteMatchingCommand("/load dad 3", CPlayerManagerImpl::LocalPlayer());
+    itsTui->ExecuteMatchingCommand("/rand -normal -tre -strict emo", CPlayerManagerImpl::LocalPlayer());
 }
 
 CAvaraAppImpl::~CAvaraAppImpl() {
