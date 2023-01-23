679f24e9bd7b4e9b84e5ba56d6ff9e5fd5e8ff4d
assertivist <andy.halstead@gmail.com>
2023-01-23 12:09:19 -0800

    Prevent overflow in PascalStrings

--- src/util/PascalStrings.h
+++ src/util/PascalStrings.h
@@ -1,4 +1,5 @@
 #include "Memory.h"
+#include <limits.h>
 
 static char *PascalStringtoCString(const unsigned char *p, int len) {
     if (len == 0) return const_cast<char *>("");
@@ -12,9 +13,15 @@ static char *PascalStringtoCString(const unsigned char *p) {
     return PascalStringtoCString(p, p[0]);
 }
 
+static size_t strnlen_s(const char* s, size_t len) {
+    size_t i = 0;
+    for (; i < len && s[i] != '\0'; ++i);
+    return i;
+}
+
 // TODO: NO! BAD!
 static unsigned char *CStringtoPascalString(const char *s) {
-    size_t len = std::strlen(s);
+    size_t len = strnlen_s(s, UCHAR_MAX);
     if (len == 0) return 0;
     unsigned char *p = new unsigned char[len + 1];
     p[0] = len;
