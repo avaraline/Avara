d3e72e8a70944d1a61be8438c833e54d00bd8c4f
assertivist <andy.halstead@gmail.com>
2023-01-23 23:35:35 -0800

    Rename strnlen_s to not conflict with mingw

--- src/util/PascalStrings.h
+++ src/util/PascalStrings.h
@@ -13,7 +13,7 @@ static char *PascalStringtoCString(const unsigned char *p) {
     return PascalStringtoCString(p, p[0]);
 }
 
-static size_t strnlen_s(const char* s, size_t len) {
+static size_t my_strnlen_s(const char* s, size_t len) {
     size_t i = 0;
     for (; i < len && s[i] != '\0'; ++i);
     return i;
@@ -21,7 +21,7 @@ static size_t strnlen_s(const char* s, size_t len) {
 
 // TODO: NO! BAD!
 static unsigned char *CStringtoPascalString(const char *s) {
-    size_t len = strnlen_s(s, UCHAR_MAX);
+    size_t len = my_strnlen_s(s, UCHAR_MAX);
     if (len == 0) return 0;
     unsigned char *p = new unsigned char[len + 1];
     p[0] = len;
