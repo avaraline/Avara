45f445a20113fa79e3576fbe25a0e4b26fb10d1c
Dan Watson <dcwatson@gmail.com>
2023-02-05 08:35:10 -0500

    Use avara socket for hole punching, remove miniupnp, clean up makefile

--- src/net/AvaraTCP.h
+++ src/net/AvaraTCP.h
@@ -42,8 +42,8 @@ void FreePacket(UDPpacket *packet);
 int ResolveHost(IPaddress *address, const char *host, uint16_t port);
 
 void RegisterPunchServer(IPaddress &localAddr);
-void RequestPunch(IPaddress &addr, uint16_t localPort);
-void Punch(int sock, IPaddress &addr);
+void RequestPunch(IPaddress &addr);
+void Punch(IPaddress &addr);
 
 int CreateSocket(uint16_t port);
 void DestroySocket(int sock);
