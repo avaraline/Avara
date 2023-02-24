742a7c3167266c4ddcbfd4dbe56feabe198ee0b4
Tom Anderson <tom@squeat.com>
2023-02-23 22:08:51 -0700

    tune the read buffer cleanup size
    seems like 4.5LT (18 queue packets) is a good balance.
    go too high and you start losing packets & aborting,
    go too low and you have to re-send more packets

--- src/net/CUDPConnection.cpp
+++ src/net/CUDPConnection.cpp
@@ -631,7 +631,8 @@ size_t CUDPConnection::ReceivedPacket(UDPPacketInfo *thePacket) {
                 }
                 // give up and forget about receiving sn=receiveSerial after awhile,
                 // players will request a frame resend and it could be coming in a later packet so skip this one
-                if (qsize >= 40) {  // LT of 8 would be 32 frame packets, plus a little for other packets
+                int PACKET_CLEANUP = 4.5/gCurrentGame->fpsScale;   // bigger is NOT better, don't go above 5-ish
+                if (qsize >= PACKET_CLEANUP) {  // LT of 8 would be 32 frame packets, plus a little for other packets
                     // call self recursively to skip receiveSerial
                     return ReceivedPacket(nullptr);
                 }
