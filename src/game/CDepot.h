32b74c85df6727ecb95091e20fd3ae2fe54389e1
assertivist <andy.halstead@gmail.com>
2023-03-21 14:55:50 -0700

    initialize part pointers to 0 in CDepot.h

--- src/game/CDepot.h
+++ src/game/CDepot.h
@@ -41,11 +41,11 @@ public:
     CSliverPart *freeSlivers[SLIVERSIZES];
     CSliverPart *activeSlivers[SLIVERSIZES];
 
-    CBSPPart *smartSight;
-    CBSPPart *smartHairs;
-    CBSPPart *grenadeSight;
-    CBSPPart *grenadeTop;
-    Boolean bspInGame;
+    CBSPPart *smartSight = 0;
+    CBSPPart *smartHairs = 0;
+    CBSPPart *grenadeSight = 0;
+    CBSPPart *grenadeTop = 0;
+    Boolean bspInGame = false;
 
     Fixed grenadePower;
     Fixed missilePower;
