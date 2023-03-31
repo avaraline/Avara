3741dae785fc989d0d027ebc7289920f87154a7a
Ryan Herriman <rherriman@gmail.com>
2023-03-30 23:07:23 -0600

    Conversions between ARGB and HSLA colors
    Also contains a preliminary implementation of
    `ARGBColor::GetContrastingShade`. Produces decent results with the
    default dashboard color, but additional testing is required to determine
    if the thresholds need adjusting.

--- src/util/ARGBColor.h
+++ src/util/ARGBColor.h
@@ -2,6 +2,7 @@
 
 #include "nanovg.h"
 
+#include <math.h>
 #include <stdint.h>
 #include <string>
 
@@ -15,11 +16,14 @@
 #  endif
 #endif
 
+class HSLAColor;
+
 class ARGBColor final {
 public:
     void* operator new(std::size_t) = delete;
     bool operator==(const ARGBColor& other) { return color == other.color; }
     constexpr ARGBColor(uint32_t value = 0x00000000): color(value) {}
+    ARGBColor(HSLAColor hsla);
     static std::optional<ARGBColor> Parse(const std::string& str);
     inline uint32_t GetRaw() { return color; }
     inline uint8_t GetA() { return color >> 24; }
@@ -32,6 +36,38 @@ public:
     inline ARGBColor WithB(uint8_t b) { return ARGBColor((color & 0xffffff00) | static_cast<uint32_t>(b)); }
     inline NVGcolor IntoNVG() { return nvgRGBA(GetR(), GetG(), GetB(), GetA()); }
     void ExportGLFloats(float *out, int n = 4);
+    ARGBColor GetContrastingShade();
 private:
     uint32_t color;
+};
+
+class HSLAColor final {
+public:
+    void* operator new(std::size_t) = delete;
+    bool operator==(const HSLAColor& other) {
+        return hue == other.hue &&
+               saturation == other.saturation &&
+               lightness == other.lightness
+               && alpha == other.alpha;
+    }
+    HSLAColor(float h, float s, float l, float a);
+    HSLAColor(ARGBColor argb);
+    inline float GetH() { return hue; }
+    inline float GetS() { return saturation; }
+    inline float GetL() { return lightness; }
+    inline float GetA() { return alpha; }
+    inline HSLAColor WithH(float h) { return HSLAColor(h, saturation, lightness, alpha); }
+    inline HSLAColor WithS(float s) { return HSLAColor(hue, s, lightness, alpha); }
+    inline HSLAColor WithL(float l) { return HSLAColor(hue, saturation, l, alpha); }
+    inline HSLAColor WithA(float a) { return HSLAColor(hue, saturation, lightness, a); }
+private:
+    float hue = 0.0;
+    float saturation = 0.0;
+    float lightness = 0.0;
+    float alpha = 0.0;
+
+    void SetH(float h);
+    inline void SetS(float s) { saturation = fmin(1.0, fmax(s, 0.0)); }
+    inline void SetL(float l) { lightness = fmin(1.0, fmax(l, 0.0)); }
+    inline void SetA(float a) { alpha = fmin(1.0, fmax(a, 0.0)); }
 };
