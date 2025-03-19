f992d308b5079ccd0bcdb93780ed1f222cfce0d9
Tom Anderson <tom@squeat.com>
2025-03-18 19:14:08 -0600

    handy script for starting/killing multiple players
    When debugging situations involving more than 2 players, it's a hassle to start them all up by hand then quit them.  This script starts however many players you want, and even better, allows you to kill them all at the end by doing a Ctrl-C in the terminal where they were started.
    
    Example: $ ./bin/mutiplayer.sh 3
    ...starts 3 instances.

--- bin/multiplayer.sh
+++ bin/multiplayer.sh
@@ -1,9 +1,8 @@
 #!/bin/sh # -x
-count=0
-if [ "$#" -lt 1 ]; then
+if [ "$#" -lt 1 ] || [ $1 -lt 2 ]; then
     echo "Usage: $0 num" >&2
-    echo "  where num = number of players" >&2
-  exit 1
+    echo "  where num = number of players (min. 2)" >&2
+    exit 1
 fi
 
 count=$1
@@ -13,16 +12,17 @@ server_options=(-s)
 client_options=(-c $host)
 both_options=(-/ "p soundVolume 5")
 
-Avara -n "Player1" "${server_options[@]}" "${both_options[@]}" -/ "p latencyTolerance 1" &
+Avara -n "Player1" "${server_options[@]}" "${both_options[@]}" &
 sleep 3
 
 for (( i=2; i < $count; ++i ))
 do
-  Avara -n "Player$i" "${client_options[@]}" "${both_options[@]}" -/ "p latencyTolerance $i" &
+  Avara -n "Player$i" "${client_options[@]}" "${both_options[@]}" &
+  sleep 1
 done
 
 # don't put the last player in the background so all can be killed with Ctrl-C
-Avara -n "Player$count" "${client_options[@]}" "${both_options[@]}" -/ "p latencyTolerance $count"
+Avara -n "Player$count" "${client_options[@]}" "${both_options[@]}"
 
 # kill all subprocesses
 pkill -P $$
