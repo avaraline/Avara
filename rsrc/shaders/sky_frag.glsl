4256722c9edda62fd31e8731618d479e18ab10df
Dan Watson <dcwatson@gmail.com>
2025-05-01 20:49:36 -0400

    Shader noise (#462)
    * Add noise to the lighting to mask some of the color banding
    
    * Noise on the ground as well
    
    * Make noise a little more pronounced
    
    * Move noise call to last
    
    ---------
    
    Co-authored-by: Ryan Herriman <rherriman@gmail.com>

--- rsrc/shaders/sky_frag.glsl
+++ rsrc/shaders/sky_frag.glsl
@@ -46,6 +46,16 @@ vec3 spec() {
     return sum;
 }
 
+/* Toned down version of Magic GLSL One-liner from:
+   https://blog.frost.kiwi/GLSL-noise-and-radial-gradient/ */
+float noise() {
+    return mix(
+        -1.0 / 255.0,
+        1.0 / 255.0,
+        fract(52.9829189 * fract(dot(gl_FragCoord.xy, vec2(0.06711056, 0.00583715))))
+    );
+}
+
 void main()
 {
     float phi = normalize(tex_coord).y;
@@ -76,5 +86,5 @@ void main()
         ),
         maxHazeDist
     );
-    color.rgb = apply_fog(color.rgb, dist);
+    color.rgb = apply_fog(color.rgb, dist) + noise();
 }
--- rsrc/shaders/world_frag.glsl
+++ rsrc/shaders/world_frag.glsl
@@ -62,6 +62,16 @@ vec3 spec() {
     return sum;
 }
 
+/* Toned down version of Magic GLSL One-liner from:
+   https://blog.frost.kiwi/GLSL-noise-and-radial-gradient/ */
+float noise() {
+    return mix(
+        -1.0 / 255.0,
+        1.0 / 255.0,
+        fract(52.9829189 * fract(dot(gl_FragCoord.xy, vec2(0.06711056, 0.00583715))))
+    );
+}
+
 vec4 light_color() {
     return mix(
         ambient * vec4(ambientColor, 1.0) * fragmentColor,
@@ -81,4 +91,5 @@ void main() {
     float yonFadeDist = objectYon - yonFadeRange;
     float alphaMult = pow(clamp((yonFadeRange + yonFadeDist - dist) / yonFadeRange, 0.0, 1.0), 0.5);
     color.a *= alphaMult;
+    color.rgb += noise();
 }
