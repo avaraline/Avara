bdc21f3c27a0758d2245a5034c50731f393b64c5
assertivist <andy.halstead@gmail.com>
2025-05-29 06:44:59 -0700

    add vcpkg as submodule, setup cmake to use vcpkg

--- bin/winavarazip.ps1
+++ bin/winavarazip.ps1
@@ -1,6 +1,7 @@
 mkdir WinAvara
-cp .\build\x64\Release\*.exe WinAvara
-cp .\build\x64\Release\*.dll WinAvara
+cp .\build\*.exe WinAvara
+cp .\build\*.dll WinAvara
 cp -r .\levels WinAvara
 cp -r .\rsrc WinAvara
+ls WinAvara
 Compress-Archive -Path .\WinAvara -DestinationPath .\WinAvara.zip
